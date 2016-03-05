4d2797eb1149091d20913c041a68850f84dacf70
sohamkamani <sohamkamani@gmail.com>
2016-03-06 04:14:36 +0530

    kpi added

--- app/scripts/controllers/analytics-controller.js
+++ app/scripts/controllers/analytics-controller.js
@@ -6,24 +6,33 @@ angular.module('instavansPorterAdminApp')
     $http.get(url + '/jobs')
       .then(function (response) {
         var data = response.data;
+        var totalWorkers = 0;
+
         data.forEach(d => {
-          d.formattedDate = moment(d.time).format('YYYY-MM-DD')
+          d.formattedDate = moment(d.time).format('YYYY-MM-DD');
+          totalWorkers += d.porters.length;
         });
 
         var groupedData = _.groupBy(data, 'formattedDate');
-
         var dates = data.map(d => d.formattedDate);
-
         var jobsPerDay = dates.map(eachDate => groupedData[eachDate].length);
 
         var workersLatePerJob = dates.map((eachDate, i) => {
           var lateWorkers = 0;
-
+          groupedData[eachDate].forEach(job => {
+            lateWorkers += job.porters.filter(porter => new Date(porter.endTime) > new Date(job.time)).length;
+          });
+          return lateWorkers;
         });
+
+        var percentageWorkersLate = _.sum(workersLatePerJob) / totalWorkers;
+        $scope.percentageWorkersLate = Math.floor(percentageWorkersLate * 1000) / 10;
 
         dates.unshift('x');
         jobsPerDay.unshift('No. of jobs');
-        var chart = c3.generate({
+        workersLatePerJob.unshift('Porters late per job');
+
+        var chart1 = c3.generate({
           bindto: '#chart',
           data: {
             x: 'x',
@@ -37,15 +46,45 @@ angular.module('instavansPorterAdminApp')
               type: 'timeseries',
               tick: {
                 format: '%Y-%m-%d',
-                rotate : 60
+                rotate: 60
               }
             },
-            y : {
-              tick : {
-                values : [0,1,2,3,4,5,6]
+            y: {
+              tick: {
+                values: [0, 1, 2, 3, 4, 5, 6]
+              }
+            }
+          }
+        });
+
+        var chart2 = c3.generate({
+          bindto: '#chart2',
+          data: {
+            colors: {
+              'Porters late per job': '#EF5350'
+            },
+            x: 'x',
+            columns: [
+              dates,
+              workersLatePerJob
+            ]
+          },
+          axis: {
+            x: {
+              type: 'timeseries',
+              tick: {
+                format: '%Y-%m-%d',
+                rotate: 60
+              }
+            },
+            y: {
+              tick: {
+                values: [0, 1, 2, 3, 4, 5, 6]
               }
             }
           }
         });
       }, 1);
+
+
   });
