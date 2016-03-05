c9e13ed9e589d4c3a2574d82ad7685b40171fb16
sohamkamani <sohamkamani@gmail.com>
2016-03-06 03:57:41 +0530

    fuk u

--- app/scripts/controllers/analytics-controller.js
+++ app/scripts/controllers/analytics-controller.js
@@ -1,15 +1,51 @@
 'use strict';
 angular.module('instavansPorterAdminApp')
   .controller('analyticsCtrl', function ($scope, $http, $timeout) {
-    $timeout(function () {
-      var chart = c3.generate({
-        bindto: '#chart',
-        data: {
-          columns: [
-            ['data1', 30, 200, 100, 400, 150, 250],
-            ['data2', 50, 20, 10, 40, 15, 25]
-          ]
-        }
-      });
-    }, 1);
+    var url = window.appUrl || 'http://localhost:1337';
+
+    $http.get(url + '/jobs')
+      .then(function (response) {
+        var data = response.data;
+        data.forEach(d => {
+          d.formattedDate = moment(d.time).format('YYYY-MM-DD')
+        });
+
+        var groupedData = _.groupBy(data, 'formattedDate');
+
+        var dates = data.map(d => d.formattedDate);
+
+        var jobsPerDay = dates.map(eachDate => groupedData[eachDate].length);
+
+        var workersLatePerJob = dates.map((eachDate, i) => {
+          var lateWorkers = 0;
+
+        });
+
+        dates.unshift('x');
+        jobsPerDay.unshift('No. of jobs');
+        var chart = c3.generate({
+          bindto: '#chart',
+          data: {
+            x: 'x',
+            columns: [
+              dates,
+              jobsPerDay
+            ]
+          },
+          axis: {
+            x: {
+              type: 'timeseries',
+              tick: {
+                format: '%Y-%m-%d',
+                rotate : 60
+              }
+            },
+            y : {
+              tick : {
+                values : [0,1,2,3,4,5,6]
+              }
+            }
+          }
+        });
+      }, 1);
   });
