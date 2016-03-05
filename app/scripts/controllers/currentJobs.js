4af90f65d609fc864b53b71a8c69b42f8223b1c6
sohamkamani <sohamkamani@gmail.com>
2016-03-05 22:15:41 +0530

    Added styles to job cards

--- app/scripts/controllers/currentJobs.js
+++ app/scripts/controllers/currentJobs.js
@@ -1,22 +1,14 @@
 'use strict';
 angular.module('instavansPorterAdminApp')
-  .controller('currentJobsCtrl', function ($scope, $http) {
+  .controller('currentJobsCtrl', function ($scope, $http, $timeout) {
       var jobs = this;
       jobs.current = [];
-      var url = 'http://localhost:1337';
+      var url = window.appUrl || 'http://localhost:1337';
       var socket = io(url);
       socket.on('job-update', function(newJobs) {
         jobs.current = newJobs;
         console.log(jobs.current);
         $scope.$digest();
-      });
-
-      $http({
-        method : 'GET',
-        url : url + '/jobs'
-      })
-      .then(function(response){
-        jobs.current = response.data;
       });
 
       jobs.styleFromJob = function(job){
@@ -35,4 +27,35 @@ angular.module('instavansPorterAdminApp')
           'background-color' : backgroundColor
         };
       };
+
+      var processJobTime = function(){
+        jobs.current.forEach(function(job){
+          var momentTime = moment(job.time);
+          job.timeTillStart = momentTime.fromNow();
+          job.prettyTime = momentTime.format('MMMM Do YYYY, h:mm a');
+        });
+      };
+
+      var processPorters = function(){
+        jobs.current.forEach(function(job){
+          job.portersAccepted = job.porters.length;
+          job.portersArriving = job.porters.filter(porter => !!porter.startTime).length;
+          job.portersReached = job.porters.filter(porter => !!porter.endTime).length;
+        });
+      };
+
+      var processJobs = function(){
+        processJobTime();
+        processPorters();
+      };
+
+      $http({
+        method : 'GET',
+        url : url + '/jobs'
+      })
+      .then(function(response){
+        jobs.current = response.data;
+        processJobs();
+      });
+      $timeout(processJobTime, 1000 * 60);
   });
